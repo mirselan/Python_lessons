3cbb79598b097799c7c5008471e0ec789eff8609
Sergei_Lanikhin <sergei.lanikhin@gmail.com>
2022-11-11 09:34:38 +0300

    check for d_number condition added

--- task401.py
+++ task401.py
@@ -1,8 +1,12 @@
 from math import pi
 
-precision = input('Enter precision(number d): ')
+precision = input('Enter precision(number d) from 10**(-1) to 10**(-10): ') # 10^{-1} ≤ d ≤10^{-10}
+d_num = float(precision)
 
-pr_len = len((precision).split('.')[1])
-num = str(pi).split('.')
-num_res = num[0] + '.' + num[1][:pr_len]
-print(num_res)
+if 10**-1 >= d_num >= 10**-10: 
+    pr_len = len((precision).split('.')[1])
+    num = str(pi).split('.')
+    num_res = num[0] + '.' + num[1][:pr_len]
+    print(num_res)
+else:
+    print('Wrong number.')
